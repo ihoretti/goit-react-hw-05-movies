b94bac7a6282187b44bca7cc1c78b794295314e0
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-01-19 20:34:46 +0200

    Create Home.js

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,46 @@
+import { toast } from 'react-toastify';
+
+import { getTrendingMovies } from 'API/fetchMovies';
+import ListMovies from 'components/ListMovies/ListMovies';
+import { useState, useEffect } from 'react';
+import { Title } from './Home.styled';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { Loader } from 'components/Loader/Loader';
+
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        setLoader(true);
+
+        const resp = await getTrendingMovies();
+        const data = resp.data.results;
+        setMovies(data);
+
+        if (data.length === 0) {
+          throw new Error('something is wrong! try again');
+        }
+      } catch (error) {
+        toast(error.message, {
+          position: toast.POSITION.BOTTOM_CENTER,
+        });
+      } finally {
+        setLoader(false);
+      }
+    };
+    getMovies();
+  }, []);
+
+  return (
+    <>
+      <Title>Trending today</Title>
+      {loader ? <Loader /> : <ListMovies movies={movies} />}
+    </>
+  );
+};
+
+export default Home;
