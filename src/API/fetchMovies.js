eff8562af56d0aae798ee8f6cddf862dbc2a9b37
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-01-19 20:34:27 +0200

    Create fetchMovies.js

---
+++ src/API/fetchMovies.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const KEY = 'eacf035cadfc4bc3c80cf18ea34eda45';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+
+export const getTrendingMovies = async () => {
+  return await axios.get(`${BASE_URL}trending/movie/day?api_key=${KEY}&page=1`);
+};
+
+export const getMovieById = async id => {
+  return await axios.get(`${BASE_URL}movie/${id}?api_key=${KEY}`);
+};
+
+export const getCredits = async id => {
+  return await axios.get(`${BASE_URL}movie/${id}/credits?api_key=${KEY}`);
+};
+
+export const getReviewsMovies = async id => {
+  return await axios.get(`${BASE_URL}movie/${id}/reviews?api_key=${KEY}`);
+};
+
+export const getSearchMovie = async query => {
+  return await axios.get(
+    `${BASE_URL}search/movie/?query=${query}&api_key=${KEY}`
+  );
+};
