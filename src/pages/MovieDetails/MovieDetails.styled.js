8c3118db77e1f1ab99b12d975bd9f9adf68e9ced
IHOR <97735057+ihoretti@users.noreply.github.com>
2023-01-19 20:35:02 +0200

    Create MovieDetails.styled.js

---
+++ src/pages/MovieDetails/MovieDetails.styled.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  width: 600px;
+`;
+
+export const PrimaryTitle = styled.h1``;
+
+export const SecondTitle = styled.h3``;
+
+export const Text = styled.p``;
+
+export const Span = styled.span``;
+
+export const AddList = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+export const ItemAddList = styled.li`
+  margin: 10px;
+  text-decoration: none;
+`;
+
+export const AddInform = styled.p`
+  margin: 0;
+  text-align: center;
+`;
